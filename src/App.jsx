b710792d2443acbf0a92cdec24dc29d75fa47ce6
LordAnthrax94 <andreagi1994@gmail.com>
2025-05-31 09:32:44 +0200

    Feat: completato Snack 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import useSwitch from "./useSwitch";
 import useDate from "./useDate";
+import useCustomPointer from "./useCustomPointer";
 
 function App() {
   const [isOn, toggle] = useSwitch();
   const currentDate = useDate();
+  const customPointer = useCustomPointer("🔥");
 
   return (
     <div>
@@ -14,7 +16,11 @@ function App() {
       <div>
         <h1>Data e ora attuali:</h1>
         <p>{currentDate.toLocaleString()}</p>
-      </div>     
+      </div> 
+      <div>
+        <h1>Sposta il mouse per vedere il cursore personalizzato!</h1>
+        {customPointer}
+      </div>   
     </div>
     
   )
